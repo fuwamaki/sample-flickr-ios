7d5e78e926ab66a5666da7efd543c8d9f093d3b0
fuwamaki <variety.fuwa.3@gmail.com>
2023-05-14 08:31:43 +0900

    refactor: rename from Main to MainViewController

--- SampleFlickr/MainViewController.swift
+++ SampleFlickr/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SampleFlickr
 //
 //  Created by fuwamaki on 2023/05/10.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
